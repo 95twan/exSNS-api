e6a0f65b18a0f7e8118a1b0f3bb1b517d4d61cf9
95twan <95twan@naver.com>
2021-02-22 19:27:56 +0900

    add api v2

--- routes/v2.js
+++ routes/v2.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
-const { verifyToken, deprecated } = require('./middlewares');
-const { Domain, User, Post, Hashtag } = require('../models');
+const { verifyToken, apiLimiter } = require('./middlewares');
+const { User, Domain, Post, Hashtag } = require('../models');
 
 const router = express.Router();
 
-router.use(deprecated);
-
-router.post('/token', async (req, res) => {
+router.post('/token', apiLimiter, async (req, res) => {
     const { clientSecret } = req.body;
     try {
         const domain = await Domain.findOne({
@@ -21,15 +19,15 @@ router.post('/token', async (req, res) => {
         if (!domain) {
             return res.status(401).json({
                 code: 401,
-                message: '등록되지 않은 도메인입니다. 먼저 도메인을 등록하세요',
+                message: '등록되지 않은 도메인입니다. 먼저 도메인을 등록해주세요',
             });
         }
         const token = jwt.sign({
             id: domain.user.id,
             nick: domain.user.nick,
         }, process.env.JWT_SECRET, {
-            expiresIn: '1m',
-            issuer: 'nodeSNS'
+            expiresIn: '30m',
+            issuer: 'nodebird',
         });
         return res.json({
             code: 200,
@@ -40,34 +38,32 @@ router.post('/token', async (req, res) => {
         console.error(err);
         return res.status(500).json({
             code: 500,
-            message: '서버 에러'
+            message: '서버 에러',
         });
     }
 });
 
-router.get('/test', verifyToken, (req, res) => {
+router.get('/test', verifyToken, apiLimiter, (req, res) => {
     res.json(req.decoded);
 });
 
-router.get('/posts/my', verifyToken, (req, res) => {
-    Post.findAll({
-        where: { userId: req.decoded.id }
-    }).then((posts) => {
-        console.log(posts);
-        res.json({
-            code: 200,
-            payload: posts,
-        });
-    }).catch((err) => {
-        console.error(err);
-        return res.status(500).json({
-            code: 500,
-            message: '서버 에러'
-        });
-    });
+router.get('/posts/my', apiLimiter, verifyToken, (req, res) => {
+    Post.findAll({ where: { userId: req.decoded.id } })
+        .then((posts) => {
+            res.json({
+                code: 200,
+                payload: posts,
+            });
+        })
+        .catch((err) => {
+            console.error(err);
+            return res.status(500).json({
+                code: 500,
+                message: '서버 에러',
+            });
+        })
 });
-
-router.get('/posts/hashtag/:title', verifyToken, async (req, res) => {
+router.get('/posts/hashtag/:title', apiLimiter, verifyToken, async (req, res) => {
     try {
         const hashtag = await Hashtag.findOne({ where: { title: req.params.title } });
         if (!hashtag) {
